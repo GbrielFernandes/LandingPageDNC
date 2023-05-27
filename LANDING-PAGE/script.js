1564a7472a91b32f7268bfaf9cdf9bc1717e6da5
GbrielFernandes <fernandesgabriel1227@gmail.com>
2023-05-27 05:02:25 -0300

    Criando uma landing Page do zero

---
+++ LANDING-PAGE/script.js
@@ -0,0 +1,17 @@
+var setaDireita = window.document.getElementById('setaDireita')
+var setaEsquerda = window.document.getElementById('setaEsquerda')
+var leonardo = window.document.getElementById('leonardo')
+var samantha = window.document.getElementById('samantha')
+var bruna = window.document.getElementById('bruna')
+function RolarParaDireita(){
+    leonardo.style = 'display:none'
+    bruna.style = 'display:flex'
+    setaDireita.style = 'display:none'
+    setaEsquerda.style = 'display:flex'
+}
+function RolarParaEsquerda(){
+    leonardo.style = 'display:flex'
+    bruna.style = 'display:none'
+    setaDireita.style = 'display:flex'
+    setaEsquerda.style = 'display:none'
+}
